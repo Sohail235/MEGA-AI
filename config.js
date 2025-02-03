741311b9b09982f5c8c475cfc8dd228cbc8206e3
Sohail235 <152407047+Sohail235@users.noreply.github.com>
2025-02-03 16:15:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '923281262584';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '' ;  // 923281262584
+global.SESSION_ID = process.env.SESSION_ID || '' ;  // uVAHTK4B#kpFE4BURtWdoP0ugmKam18SxqiOmJJX6AFBCkoxCozk
 
 global.mods = []
 global.prems = []
